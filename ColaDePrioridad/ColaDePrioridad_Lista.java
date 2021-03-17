62ab564b284dabf103971ba8588db23d15e9a4d2
Joaquin Vigna <joaquin@brokenrubik.co>
2021-03-17 16:44:29 -0300

    Cola de prioridad + Heap

---
+++ ColaDePrioridad/ColaDePrioridad_Lista.java
@@ -0,0 +1,123 @@
+import java.util.Scanner;
+
+abstract class ColaDePrioridad {
+    abstract void insertar(int elemento);
+    abstract void desencolar();
+    abstract int tope();
+    abstract boolean esVacia();
+    abstract boolean estaLlena();
+};
+
+class NodoLista {
+    public NodoLista sig;
+    public NodoLista ant;
+    public int dato;
+    public NodoLista() {};
+    public NodoLista(int unDato, NodoLista unSig, NodoLista unAnt) {
+        dato = unDato;
+        sig = unSig;
+        ant = unAnt;
+    };
+    public NodoLista(int unDato) {
+        dato = unDato;
+        sig = null;
+        ant = null;
+    };
+};
+
+class Lista {
+    private NodoLista ppio;
+
+    public Lista() { ppio = null; }
+
+    public void insertar(int dato) {
+        NodoLista nuevoNodo =  new NodoLista(dato, this.ppio, null);
+        if(this.ppio != null) {
+            this.ppio.ant = nuevoNodo;
+        }
+        ppio = nuevoNodo;
+    }
+    public int obtenerMenor() {
+		if(esVacia())
+			throw new Error("La lista esta vacia");
+        NodoLista menor = this.ppio;
+        NodoLista aux = this.ppio;
+        while(aux != null) {
+            if(menor.dato > aux.dato) {
+                menor = aux;
+            }
+            aux = aux.sig;
+        }
+        return menor.dato;
+    }
+
+    public void eliminarMenor() {
+		if(esVacia())
+			throw new Error("La lista esta vacia");
+        NodoLista menor = this.ppio;
+        NodoLista aux = this.ppio;
+        while(aux != null) {
+            if(menor.dato > aux.dato) {
+                menor = aux;
+            }
+            aux = aux.sig;
+        }
+
+        // elimino el nodo que contiene el menor dato
+        if(menor.ant != null) {
+            menor.ant.sig = menor.sig;
+        }else {
+            ppio = menor.sig;
+        }
+        if(menor.sig != null) {
+            menor.sig.ant = menor.ant;
+        }
+    }
+
+    public boolean esVacia() {
+        return ppio == null;
+    }
+};
+
+class ColaDePrioridadImpLista extends ColaDePrioridad {
+    private Lista miLista;
+
+    public ColaDePrioridadImpLista() {
+        miLista = new Lista();
+    }
+    public void insertar(int el) {
+        miLista.insertar(el);
+    }
+    public void desencolar() {
+        miLista.eliminarMenor();
+    }
+    public int tope() {
+		return miLista.obtenerMenor();
+	}
+    public boolean esVacia() {
+        return miLista.esVacia();
+    }
+    public boolean estaLlena() {
+        return false;
+    }
+};
+
+
+
+public class ColaDePrioridad_Lista {
+    public static void main(String[] args) {
+        ColaDePrioridad cp = new ColaDePrioridadImpLista();
+		
+        cp.insertar(12);
+        cp.insertar(1);
+        cp.insertar(44);
+        cp.insertar(100);
+        cp.insertar(56);
+        cp.insertar(122);
+
+        while(!cp.esVacia()) {
+            System.out.println(cp.tope());
+			cp.desencolar();
+        }
+    }
+}
---
+++ ColaDePrioridad/Heap.java
@@ -0,0 +1,134 @@
+import java.util.Scanner;
+
+class MinHeap {
+    private int[] elementos;
+    private int largo;
+    private int ultimoLibre;
+
+    public MinHeap(int tamanio) {
+        elementos = new int[tamanio + 1];
+        largo = tamanio;
+        ultimoLibre = 1;
+    }
+
+    private int izq(int nodo) {
+        return nodo * 2;
+    }
+
+    private int der(int nodo) {
+        return nodo * 2 + 1;
+    }
+
+    private int padre(int nodo) {
+        return nodo/2;
+    }
+
+    private void intercambiar(int x, int y) {
+        int aux = elementos[x];
+        elementos[x] = elementos[y];
+        elementos[y] = aux;
+    }
+
+    public void insertar(int nuevoElemento) {
+        if(!estaLleno()) {
+            elementos[ultimoLibre] = nuevoElemento;
+            flotar(ultimoLibre);
+            ultimoLibre++;
+        }
+    }
+
+    private void flotar(int nodo) {
+        if(nodo != 1) {
+            int nodoPadre = padre(nodo);
+            if(elementos[nodo] < elementos[nodoPadre]) {
+                intercambiar(nodo, nodoPadre);
+                flotar(nodoPadre);
+            }
+        }
+    }
+
+    public int obtenerMinimo() {
+        if(!esVacio()) {
+            return elementos[1];
+        }
+        return -1;
+    }
+
+    public void borrarMinimo() {
+        if(!esVacio()) {
+            int ret = elementos[1];
+            elementos[1] = elementos[ultimoLibre - 1];
+            ultimoLibre--;
+            hundir(1);
+        }
+    }
+
+    private void hundir(int nodo) {
+        // si tiene hijos (al menos 1)
+        if(izq(nodo) < ultimoLibre) {
+            int izq = izq(nodo);
+            int der = der(nodo);
+            int hijoMenor = izq;
+
+            if(der < ultimoLibre && elementos[der] < elementos[izq]) {
+                hijoMenor = der;
+            }
+
+            if(elementos[hijoMenor] < elementos[nodo]) {
+                intercambiar(hijoMenor, nodo);
+                hundir(hijoMenor);
+            }
+        }
+    }
+
+    public boolean esVacio() {
+        return ultimoLibre == 1;
+    }
+
+    public boolean estaLleno() {
+        return ultimoLibre == largo;
+    }
+
+    public void imprimirHeap() {
+        System.out.println("Array: ");
+        for(int i=1; i < ultimoLibre; i++) {
+            System.out.print(elementos[i] + " ");
+        }
+        System.out.println("");
+        System.out.println("Arbol:");
+        int cantidadPorNivel = 1;
+        int impresosDelNivel = 0;
+        for(int i=1; i < ultimoLibre; i++) {
+            System.out.print(elementos[i] + " ");
+            impresosDelNivel++;
+            if(cantidadPorNivel == impresosDelNivel) {
+                System.out.println("");
+                cantidadPorNivel *= 2;
+                impresosDelNivel = 0;
+            }
+        }
+		System.out.println("\n");
+    }
+}
+
+public class Heap {
+    public static void main(String[] args) {
+        MinHeap heap = new MinHeap(20);
+        heap.insertar(5);
+        heap.insertar(2);
+        heap.insertar(3);
+        heap.insertar(64);
+        heap.insertar(34);
+        heap.insertar(100);
+        heap.insertar(23);
+        heap.insertar(21);
+
+        heap.imprimirHeap();
+
+        System.out.println("Vacio el heap:");
+        while(!heap.esVacio()) {
+            System.out.println(heap.obtenerMinimo());
+			heap.borrarMinimo();
+        }
+    }
+}
