4198c7bf3b58d68e8137cedf8915a7db8954ca9b
Joaquin Vigna <joaquin@brokenrubik.co>
2021-05-31 17:50:49 -0300

    format

--- Memoizacion/Mochila01.java
+++ Memoizacion/Mochila01.java
@@ -1,30 +1,28 @@
-import java.util.*; 
-import java.lang.Math;   
+import java.util.*;
+import java.lang.Math;
 
-class Mochila01 { 
+class Mochila01 {
 
-    static int max(int a, int b) 
-    { 
-        return (a > b) ? a : b; 
-    } 
+    static int max(int a, int b) {
+        return (a > b) ? a : b;
+    }
 
     public static int generarRandom(int max, int min) {
-        return (int)(Math.random() * (max - min + 1)) + min;  
+        return (int) (Math.random() * (max - min + 1)) + min;
     }
-  
-    public static void main(String args[]) 
-    { 
+
+    public static void main(String args[]) {
         System.out.println("Ingrese cantidad de elementos");
-        Scanner in =  new Scanner(System.in);
+        Scanner in = new Scanner(System.in);
         int cantElementos = in.nextInt();
 
-        int[] valores = new int[cantElementos];; 
-        int[] pesos= new int[cantElementos];
+        int[] valores = new int[cantElementos];
+        int[] pesos = new int[cantElementos];
         int capacidadMochila = 50;
 
-        for(int i=0; i< cantElementos; i++) {
+        for (int i = 0; i < cantElementos; i++) {
             valores[i] = generarRandom(1, 10);
             pesos[i] = generarRandom(1, 100);
         }
-    } 
+    }
 }
