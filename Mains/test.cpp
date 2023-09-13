b2399aba334bf9d689057bb3cbd4752b2d67ca35
Joaquin Vigna <joaquin@brokenrubik.co>
2023-09-13 17:22:01 -0300

    cambiar DFS y BFS a funcion lambda

--- Mains/test.cpp
+++ Mains/test.cpp
@@ -2,22 +2,27 @@
 #include <string>
 #include <iostream>
 #include <limits>
+#include "../ADTs/Graph.h"
+#include "../ADTs/GraphListImp.cpp"
 using namespace std;
-
-#include "../DataStructures/AVL.cpp"
-
-void imprimirFn(int el) {
-    cout << el << endl;
-}
 
 int main()
 {
-    AVL<int> * arbolito = new AVL<int>();
-    arbolito->insert(20);
-    arbolito->insert(80);
-    arbolito->insert(10);
-    arbolito->insert(5);
-    arbolito->insert(300);
+    Graph *graph = new GraphListImp(5, false, false);
+    graph->addEdge(1, 2);
+    graph->addEdge(1, 3);
+    graph->addEdge(2, 4);
+    graph->addEdge(3, 4);
+    graph->addEdge(4, 5);
 
-    arbolito->inOrder(imprimirFn);
+    int *distance = new int[graph->getV() + 1];
+    graph->BFS(1, [&distance](int vertex, int step) {
+        distance[vertex] = step;
+    });
+
+    for (int i = 1; i <= graph->getV(); i++)
+    {
+        cout << "Distance from 1 to " << i << ": " << distance[i] << endl;
+    }
+    return 0;
 }
