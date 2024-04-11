4031190eaab57a73de5d059e0b4a1114b95cb7bd
Joaquin Vigna <joaquin@brokenrubik.co>
2024-04-11 18:09:56 -0300

    dijkstra

--- clase/Grafo_LA.cpp
+++ clase/Grafo_LA.cpp
@@ -1,8 +1,10 @@
 #include <string>
 #include <iostream>
-#include <limits>
+#include <climits>
 #include "../ADTs/Queue.h"
 #include "../ADTs/QueueImp.cpp"
+
+#define INF INT_MAX;
 
 using namespace std;
 
@@ -89,27 +91,100 @@ bool* bfs(int nodo, Grafo_LA * g, bool* encolados) {
 }
 
 
-int main()
-{
-    Grafo_LA * miGrafito = new Grafo_LA(5, false);
-    miGrafito->agregarArista(0, 1);
-    miGrafito->agregarArista(1, 2);
-    miGrafito->agregarArista(3, 4);
-    miGrafito->agregarArista(2, 4);
-
-
-    int cantComponentesConexas = 0;
-    bool* visitados = new bool[miGrafito->getV()]();
-    for (int i = 0; i < miGrafito->getV(); i++){
-        visitados[i] = false;
+void recostruirCamino(int destino, int * vengo) {
+    if(destino != -1) {
+        recostruirCamino(vengo[destino], vengo);
+        cout << destino << " ";
     }
-    for (int i = 0; i < miGrafito->getV(); i++)
+}
+
+void dijsktra(int origen, Grafo_LA *g) {
+    int* costo = new int[g->getV()]();
+    int* vengo = new int[g->getV()]();
+    bool* visitado = new bool[g->getV()]();
+
+    for (int i = 0; i < g->getV(); i++)
     {
-        if(!visitados[i]) {
-            cantComponentesConexas++;
-            bfs(i, miGrafito, visitados);
+        costo[i] = INF;
+        vengo[i] = -1;
+        visitado[i] = false;
+    }
+
+    costo[origen] = 0;
+    
+    for (int i = 0; i < g->getV(); i++)
+    {
+        // vertice a procesar: el de menor costo NO visitado
+        int v = -1;
+        int menorCosto = INF;
+        for (int j = 0; j < g->getV(); j++)
+        {
+            if(costo[j] < menorCosto && !visitado[j]) {
+                v = j;
+                menorCosto = costo[j];
+            }
+        }
+        
+        // nos cubrimos por si el menor nodo no visitado no lo puedo alcanzar me voy
+        if(v == -1)
+            break;
+        
+        visitado[v] = true;
+
+        NodoLista<Arista> * ady = g->adyacentesA(v);
+        while(ady != NULL) {
+            Arista a = ady->dato;
+            int u = a.destino;
+            int w = a.peso;
+            if(!visitado[u] && costo[u] > costo[v] + w) {
+                costo[u] = costo[v] + w;
+                vengo[u] = v;
+            }
+            ady = ady->sig;
         }
     }
 
-    cout << "La cantidad de componentes conexas es " << cantComponentesConexas << endl;
+    std::cout << "Desde el origen " << origen << std::endl;
+    for (int i = 0; i < g->getV(); i++)
+    {
+        if(!visitado[i]) {
+            std::cout << "No puedo llegar a " << i << std::endl;
+        }else {
+            std::cout << "El costo de llegar a " << i << " es: " << costo[i] << std::endl;
+            recostruirCamino(i, vengo);
+            std::cout  << std::endl;
+        }
+    }
+}
+
+
+int main()
+{
+    Grafo_LA * miGrafito = new Grafo_LA(5, true);
+    miGrafito->agregarArista(0, 4, 2);
+    miGrafito->agregarArista(1, 0, 2);
+    miGrafito->agregarArista(1, 3, 4);
+    miGrafito->agregarArista(1, 2, 1);
+    miGrafito->agregarArista(2, 3, 2);
+    miGrafito->agregarArista(3, 4, 8);
+    miGrafito->agregarArista(3, 2, 0);
+
+
+    dijsktra(1, miGrafito);
+
+
+    // int cantComponentesConexas = 0;
+    // bool* visitados = new bool[miGrafito->getV()]();
+    // for (int i = 0; i < miGrafito->getV(); i++){
+    //     visitados[i] = false;
+    // }
+    // for (int i = 0; i < miGrafito->getV(); i++)
+    // {
+    //     if(!visitados[i]) {
+    //         cantComponentesConexas++;
+    //         bfs(i, miGrafito, visitados);
+    //     }
+    // }
+
+    // cout << "La cantidad de componentes conexas es " << cantComponentesConexas << endl;
 }
